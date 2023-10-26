504a0a49ee2ac9fdc2787f99dbf6eed2b92831b2
Artem-Antonian <149022931+Artem-Antonian@users.noreply.github.com>
2023-10-25 20:10:47 -0400

    Add files via upload

---
+++ Encoder.py
@@ -0,0 +1,38 @@
+
+game = True
+
+def encode():
+    password = ""
+    user = []
+    user_input = input("Please enter your password: ")
+    user.append(user_input)
+    print("Your password has been encoded and stored!")
+    print("")
+    print(user[0])
+    for i in user[0]:
+        encoded_num = str(int(i) + 3)
+        password += encoded_num
+def main(game):
+    while game:
+
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        print("")
+
+        option = int(input("PLease enter an option: "))
+
+        if option == 1:
+            encode()
+        elif option == 2:
+            pass
+        else:
+            game = False
+
+
+
+
+if __name__ == '__main__':
+    main(game)
